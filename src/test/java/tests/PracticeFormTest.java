032155e9bfad2a8a524e9e95627cca3b06b1955c
Ruslan Tugushev <rutug.official@gmail.com>
2025-06-18 03:36:44 +0400

    add part of future auto test

---
+++ src/test/java/tests/PracticeFormTest.java
@@ -0,0 +1,71 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+//import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class PracticeFormTest {
+
+    @BeforeAll
+    static void SetUp(){
+
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        //Configuration.holdBrowserOpen = true; - браузер не будет закрываться после выполнения тестов
+        //Configuration.timeout = 5000; - корректировка таймаута
+    }
+
+
+    @Test
+    void fillFormTest() {
+
+        //указываем нужную ссылку
+        open("/automation-practice-form");
+
+        //Указываем фамилию, имя и почту
+        $("#firstName").setValue("Ivan");
+        $("#lastName").setValue("Ivanov");
+        $("#userEmail").setValue("Ivanov@123.com");
+
+        //Указываем номер тлф
+        $("#userNumber").setValue("89999990000");
+
+        //Указываем адрес
+        $("#currentAddress").setValue("Some address 123");
+
+        //Указываем пол
+        $("#gender-radio-1").parent().click();
+
+        //Указываем дату
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("February");
+        $(".react-datepicker__year-select").selectOption("1999");
+        $(".react-datepicker__day--024").click();
+
+        //Указываем предметы
+        $("#subjectsInput").setValue("Physics").pressEnter();
+        $("#subjectsInput").setValue("English").pressEnter();
+
+
+        //Указываем хобби
+        $("#hobbies-checkbox-1").parent().click();
+
+        //Загружаем картинку
+        $("#uploadPicture").uploadFromClasspath("Img/photo_2022-12-19_20-56-33.jpg");
+        //$("#uploadPicture").uploadFile(new File("src/test/resources/Img/photo_2022-12-19_20-56-33.jpg")); - один из вариантов загрузки
+
+
+
+
+        $("").shouldHave(text(""));
+        $("").shouldHave(text(""));
+        $("").shouldHave(text(""));
+        $("").shouldHave(text(""));
+    }
+}
